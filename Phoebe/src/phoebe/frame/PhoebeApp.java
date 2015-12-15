625c090987f36b86fb0ec6db721b0661072d4824
coffee <786792111@qq.com>
2015-12-15 18:40:48 +0800

    application 定义。新增一份文档

---
+++ Phoebe/src/phoebe/frame/PhoebeApp.java
@@ -0,0 +1,15 @@
+package phoebe.frame;
+
+import android.app.Application;
+
+/**
+ * 这个类中主要做一些全局的配置, 或者整个app广泛用到的方法逻辑<br>
+ * 
+ * 生命周期从
+ * 
+ * @author coffee<br>
+ *         2015-12-15下午3:28:30
+ */
+public class PhoebeApp extends Application {
+	
+}
