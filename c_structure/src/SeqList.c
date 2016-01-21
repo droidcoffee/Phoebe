213e7f22fe53fb32a5f1aabb03a02c834d0913f1
coffee <786792111@qq.com>
2016-01-21 23:52:24 +0800

    重新提交工程

---
+++ c_structure/src/SeqList.c
@@ -0,0 +1,74 @@
+/*
+ * SeqList.c
+ * 线性表的相关操作
+ *  Created on: 2016-1-20
+ *      Author: coffee
+ */
+
+#include <stdio.h>
+
+#define ListSize 6
+typedef int DataType;
+
+struct SeqList {
+	DataType data[ListSize]; //数据data用来存放结点
+	int length;	// 线性表的当前长度
+};
+
+/**
+ * 插入元素
+ *
+ * @param positon 指定的位置 , 从0开始
+ */
+void insertList(struct SeqList * L, int position, DataType data) {
+	//当 position == L->length + 1的时候表示在最后一个地方插入
+	if (position < 0 || position > L->length) {
+		printf("%d \t %s\n", position, "位置非法");
+		return;
+	}
+	if (L->length >= ListSize) {
+		printf("%s\n", "列表已满");
+		return;
+	}
+	int j;
+	for (j = L->length; j > position; j--) {
+		L->data[j] = L->data[j - 1];
+	}
+	L->data[position] = data;
+	L->length++;
+}
+
+void delete(){
+	// test
+}
+
+/**
+ * 打印SeqList的所有元素
+ */
+void printSeqList(struct SeqList * L) {
+	int i = 0;
+	for (i = 0; i < L->length; i++) {
+		printf("%d ", L->data[i]);
+	}
+	printf("\n");
+}
+/**
+ * 测试插入元素
+ */
+void testInsertSeqList() {
+	struct SeqList s = { .length = 5, .data = { 1, 3, 5, 7, 9 } };
+	insertList(&s, -1, 2);	//位置非法
+	insertList(&s, 6, 2);	//位置非法
+	printSeqList(&s);
+	insertList(&s, 0, 2);	//0位置插入元素2
+	insertList(&s, 1, 11);	//1位置插入元素2
+	insertList(&s, 5, 55);	//1位置插入元素2
+
+	printSeqList(&s);
+}
+
+int main() {
+	testInsertSeqList();
+	return 0;
+}
+
